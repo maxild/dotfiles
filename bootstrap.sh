5bd4be56e37ef0ee0b82682e361022f90b18f3a7
maxild <mmaxild@gmail.com>
2015-01-26 11:59:27 +0100

    do not exclude sublime folder any more

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,19 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-        --exclude "scripts" --exclude="sublime" --exclude "docs" \
-        --exclude "README.md" --exclude "LICENSE-MIT.txt" \
+  # '.git/' excludes any .git directory in ../dotfiles tree.
+  # '.DS_Store' excludes any Finder created dot file in ../dotfiles tree.
+  # '/bootstrap.sh' excludes only this file.
+  # '/scripts/' excludes ./scripts directory (i.e. only ../dotfiles/scripts/ folder).
+  # etc....
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "/bootstrap.sh" \
+        --exclude "/scripts/" --exclude "/docs/" \
+        --exclude "/README.md" --exclude "/LICENSE-MIT.txt" \
         -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" || "$1" == "-f" ]]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
@@ -21,4 +26,5 @@ else
 		doIt;
 	fi;
 fi;
-unset doIt;
+
+unset -f doIt;
